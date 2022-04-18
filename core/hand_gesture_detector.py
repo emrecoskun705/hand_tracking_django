f2c1bc191ec1d890ed7fd39b8efd199dfad7291c
emrecoskun705 <emrecoskun705@gmail.com>
2022-04-18 23:08:16 +0300

    update files and add ml model for hand gestures

---
+++ core/hand_gesture_detector.py
@@ -0,0 +1,10 @@
+import os
+from django.conf import settings
+import joblib
+
+hand_gesture_detector = os.path.join(settings.BASE_DIR, 'ml_model')
+
+
+def detector(data):
+    loaded_model = joblib.load(hand_gesture_detector)
+    return loaded_model.predict(data)[0]
